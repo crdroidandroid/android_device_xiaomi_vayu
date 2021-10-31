2e43ac14e24380821fb3679ee272f8d26a3e9018
Pranav Vashi <neobuddy89@gmail.com>
2021-10-31 21:37:13 +0530

    vayu: Merge overlay resources
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I5c2634966fb9d09d361e07c7ac677db7391d96de

--- device.mk
+++ device.mk
@@ -11,8 +11,7 @@ $(call inherit-product, device/xiaomi/sm8150-common/msmnile.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
