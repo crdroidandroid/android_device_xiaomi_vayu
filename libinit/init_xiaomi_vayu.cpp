01c16b50bc23d0a78bd3da093e54427c24c6ac3a
Pranav Vashi <neobuddy89@gmail.com>
2022-11-06 18:21:18 +0530

    vayu: Update build fingerprints
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libinit/init_xiaomi_vayu.cpp
+++ libinit/init_xiaomi_vayu.cpp
@@ -17,7 +17,7 @@ static const variant_info_t vayu_global_info = {
     .device = "vayu",
     .marketname = "POCO X3 Pro",
     .model = "M2102J20SI",
-    .build_fingerprint = "Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.5.0.SJUMIXM:user/release-keys",
+    .build_fingerprint = "Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.8.0.SJUMIXM:user/release-keys",
 
     .nfc = true,
 };
