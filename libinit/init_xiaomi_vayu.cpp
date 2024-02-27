35c232c65751322e3f9c7f0ff16e3c5f19b9a1bd
bibarub <wi1717171713@gmail.com>
2024-02-28 00:23:15 +0300

    vayu: libinit: proper props for bhima
    * vayu_eea variant was also removed
    * because the "EEA" HWC value does not seem to actually exist

--- libinit/init_xiaomi_vayu.cpp
+++ libinit/init_xiaomi_vayu.cpp
@@ -9,9 +9,7 @@
 
 #include "vendor_init.h"
 
-#define FINGERPRINT "POCO/vayu_global/vayu:13/RKQ1.200826.002/V14.0.3.0.TJUMIXM:user/release-keys"
-
-static const variant_info_t vayu_global_info = {
+static const variant_info_t vayu_info = {
     .hwc_value = "GLOBAL",
     .sku_value = "",
 
@@ -20,21 +18,7 @@ static const variant_info_t vayu_global_info = {
     .mod_device = "vayu_global",
     .marketname = "POCO X3 Pro",
     .model = "M2102J20SG",
-    .build_fingerprint = FINGERPRINT,
-
-    .nfc = true,
-};
-
-static const variant_info_t vayu_eea_info = {
-    .hwc_value = "EEA",
-    .sku_value = "",
-
-    .brand = "POCO",
-    .device = "vayu",
-    .mod_device = "vayu_eea_global",
-    .marketname = "POCO X3 Pro",
-    .model = "M2102J20SG",
-    .build_fingerprint = FINGERPRINT,
+    .build_fingerprint = "POCO/vayu_global/vayu:13/RKQ1.200826.002/V14.0.3.0.TJUMIXM:user/release-keys",
 
     .nfc = true,
 };
@@ -45,17 +29,16 @@ static const variant_info_t bhima_info = {
 
     .brand = "POCO",
     .device = "bhima",
-    .mod_device = "vayu_in_global",
+    .mod_device = "bhima_in",
     .marketname = "POCO X3 Pro",
     .model = "M2102J20SI",
-    .build_fingerprint = FINGERPRINT,
+    .build_fingerprint = "POCO/bhima_in/bhima:13/RKQ1.200826.002/V14.0.2.0.TJUINXM:user/release-keys",
 
     .nfc = false,
 };
 
 static const std::vector<variant_info_t> variants = {
-    vayu_global_info,
-    vayu_eea_info,
+    vayu_info,
     bhima_info,
 };
 
