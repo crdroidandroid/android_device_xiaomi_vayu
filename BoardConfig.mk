64cbf786e2e12d0f8de512640d403f008f48a823
Pranav Vashi <neobuddy89@gmail.com>
2024-10-10 01:02:45 +0530

    vayu: manifest: Add fx.tunnel entry for fingerprint
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,9 @@ TARGET_PROVIDES_AUDIO_EXTNS := true
 # Display
 TARGET_SCREEN_DENSITY := 410
 
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_vayu
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_vayu
