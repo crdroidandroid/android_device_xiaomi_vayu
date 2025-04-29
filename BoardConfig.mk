e8cb8bb96e99d34656b72d724348e56e14336cb8
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:21:39 +0100

    vayu: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,6 @@ TARGET_SCREEN_DENSITY := 410
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_vayu
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_vayu
 
 # Kernel
--- device.mk
+++ device.mk
@@ -44,6 +44,9 @@ PRODUCT_PACKAGES += \
     libkeymaster_messages.vendor \
     vendor.xiaomi.hardware.fx.tunnel@1.0.vendor
 
+# Init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_vayu)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
