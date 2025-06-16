e381a77c004b813d2d48f4cb272b98ee563228dc
Bin <9633157+web1n@users.noreply.github.com>
2025-06-16 23:40:24 +0800

    vayu: Patch audio to load libaudioroute-v34.so
    Change-Id: Idad20512982b8753820fffa6d956df137e462638
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -39,6 +39,8 @@ blob_fixups: blob_fixups_user_type = {
             b'\x73\x74\x5F\x6C\x69\x63\x65\x6E\x73\x65\x2E\x6C\x69\x63',
             b'\x63\x61\x6D\x65\x72\x61\x5F\x63\x6E\x66\x2E\x74\x78\x74',
         ),
+    ('vendor/lib/hw/audio.primary.msmnile.so', 'vendor/lib/libaudioroute_ext.so'): blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
